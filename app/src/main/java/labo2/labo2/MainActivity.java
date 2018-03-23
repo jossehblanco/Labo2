f8b56c613794455fb38f838b4971f4a6db5315f0
jossehblanco <jossehblanco@gmail.com>
2018-03-23 14:11:30 -0600

    Se removieron metodos que no se utilizaran

--- app/src/main/java/labo2/labo2/MainActivity.java
+++ app/src/main/java/labo2/labo2/MainActivity.java
@@ -12,15 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-    }
-
-    public void changeText(View v){
-        String aux;
-        txt1 = (TextView) findViewById(R.id.txt1);
-        txt2 = (TextView) findViewById(R.id.txt2);
-        aux = (String)txt1.getText();
-        txt1.setText(txt2.getText());
-        txt2.setText(aux);
     }
 }
